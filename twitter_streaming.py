ae62edaf24144d7277001c229d9e34ce46f76e4c
karaiba <karaibalazs@hotmail.hu>
2017-07-28 15:05:47 +0200

    twitter_streaming.py

---
+++ twitter_streaming.py
@@ -0,0 +1,28 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+access_token = "240814375-S5Us4PODFQV73GJAYlt3neMK2e5K0UQ6aFItrn9T"
+access_token_secret = "Mj20HMHxNkwTVK6wYL4z8kKfDITOeK0fnnDUGC1BAAZYq"
+consumer_key = "8USNprCJgfw7zgCBsoM1twTZ1"
+consumer_secret = "M5fWOB7iJojKLuQT5y7yENOwj0kD0xwyECR3AO5x7OkrhsFoPJ"
+
+
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print data
+        return True
+
+    def on_error(self, status):
+        print status
+
+
+if __name__ == '__main__':
+
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    stream.filter(track=['python', 'javascript', 'ruby'])
